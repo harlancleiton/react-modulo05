b23af16c7a0d3f4543d37842825f306dc5d5322c
Harlan Cleiton <harlancleiton@gmail.com>
2019-10-22 22:12:14 -0300

    Main Styles

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton, Title } from './styles';
 
 export default function Main() {
   return (
     <Container>
-      <Title>Main</Title>
+      <Title>
+        <FaGithubAlt />
+        Repositórios
+      </Title>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Pesquisar repositório" />
+        <SubmitButton>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
     </Container>
   );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,10 +1,46 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  margin: 80px auto;
 `;
 
 export const Title = styled.h1`
-  font-size: 24px;
-  color: #fff;
+  font-size: 20px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  svg {
+    margin-right: 10px;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  margin-top: 30px;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({ type: 'submit' })`
+  background: #7159c1;
+  border: 0;
+  padding: 0 15px;
+  margin-left: 10px;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
