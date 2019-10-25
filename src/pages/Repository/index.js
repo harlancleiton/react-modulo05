daa40c738926868f90095e83bc97b42949d4b107
Harlan Cleiton <harlancleiton@gmail.com>
2019-10-24 23:31:05 -0300

    Base Repository page

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
-export default function Repository() {
-  return (
-    <div>
-      <h1>Repository</h1>
-    </div>
-  );
+export default class Repository extends Component {
+  // static propTypes = {
+  //   match: PropTypes.shape({
+  //     params: PropTypes.shape({ repository: PropTypes.string }),
+  //   }).isRequired(),
+  // };
+
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired(),
+  };
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { params } = this.props.match;
+    const decodedName = decodeURIComponent(params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`repos/${decodedName}`),
+      api.get(`repos/${decodedName}/issues`, {
+        params: { state: 'open', per_page: 5 },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+
+    return (
+      <div>
+        <h1>Repository</h1>
+      </div>
+    );
+  }
 }
